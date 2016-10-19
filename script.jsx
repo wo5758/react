c3636dce35749b9676ea61fdec246f6d69ad2208
wo5758 <wo5758@naver.com>
2016-10-19 09:45:29 +0900

    react

---
+++ script.jsx
@@ -0,0 +1,12 @@
+var App = React.createClass({
+    render: function(){
+        return (
+            <h1>Hello!</h1>
+        );
+    }
+});
+ReactDOM.render(<App/>,document.getElementById("root"));
+//ReactDOM.render(
+//    <h1>hello</h1>,
+//    document.getElementById("root")
+//);
